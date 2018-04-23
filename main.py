eec662eb6c447b6e2be041caf2cd43823847b9ad
kaevupoiss <rasmus.talioja@gmail.com>
2018-04-23 22:01:17 +0300

    Working demo

--- main.py
+++ main.py
@@ -9,24 +9,43 @@ def get_dares():
 @app.route("/")
 def dare():
     req_data = get_dares()
+    randq = randint(0, 2)
+
     newdare = ""
     checked1 = ""
     checked2 = ""
     checked3 = ""
+    checked4 = ""
+    checked5 = ""
     level = ""
+    tod = 'truths'
+
+    if request.args.get('tod') == "Truth":
+        tod = 'truths'
+    elif request.args.get('tod') == "Dare":
+        tod = 'dares'
+
     if request.args.get('level'):
         level = int(request.args.get('level'))
     randq = randint(0, 2)
+
     if level == 1:
-        newdare = req_data['levels'][level - 1]['dares'][randq]
+        newdare = req_data['levels'][level - 1][tod][randq]
         checked1 = "checked"
     elif level == 2:
-        newdare = req_data['levels'][level - 1]['dares'][randq]
+        newdare = req_data['levels'][level - 1][tod][randq]
         checked2 = "checked"
     elif level == 3:
-        newdare = req_data['levels'][level - 1]['dares'][randq]
+        newdare = req_data['levels'][level - 1][tod][randq]
         checked3 = "checked"
-    return render_template('main.html', newdare = newdare, checked1 = checked1, checked2 = checked2,  checked3 = checked3)
+    elif level == 4:
+        newdare = req_data['levels'][level - 1][tod][randq]
+        checked4 = "checked"
+    elif level == 5:
+        newdare = req_data['levels'][level - 1][tod][randq]
+        checked5 = "checked"
+
+    return render_template('main.html', newdare = newdare, checked1 = checked1, checked2 = checked2,  checked3 = checked3, checked4 = checked4, checked5 = checked5)
 
 if __name__ == "__main__":
     app.run(debug=True)
