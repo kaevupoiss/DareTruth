8e0acff5e9e46096a79502b6b816058b7e2c64bf
Rasmus Talioja <rtalioja@mercans.com>
2024-02-18 23:21:37 +0200

    Working version

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,42 @@
+import type { PageServerLoad } from './$types';
+import type { Actions } from './$types';
+
+export const load: PageServerLoad = (async () => {
+    return {};
+})
+
+export const actions = {
+    default: async ({ request, fetch }) => {
+        const data = await request.formData()
+
+        const level = Number(data.get('level'))
+        const tod = data.get('tod')
+
+        if (!level || !tod) {
+            return
+        }
+
+        if (tod !== 'truths' && tod !== 'dares') {
+            return
+        }
+
+        const dares = await fetch('dares.json').then(res => res.json())
+
+        const levelItems = dares.levels[level - 1]
+        const daresToChooseFrom = levelItems[tod]
+
+        let newDare = daresToChooseFrom[Math.floor(Math.random() * daresToChooseFrom.length)]
+
+        console.log();
+        
+
+        if (level >= 2) {
+            const chance = Math.floor(Math.random() * 100);
+
+            if (chance <= 2 * (level - 2))
+                newDare = "Special Dare: Remove one piece of clothing"
+        }
+
+        return { newDare, level }
+    }
+} satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import '$lib/style.css';
+
+	const { form } = $props();
+</script>
+
+<div class="wrapper">
+	<h1 class="main">DARETRUTH</h1>
+
+	<form
+		action="/"
+		method="POST"
+		use:enhance={() => {
+			return async ({ update }) => {
+				update({ reset: false });
+			};
+		}}
+	>
+		<div class="level">
+			{#each [1, 2, 3, 4, 5] as index}
+				<input
+					type="radio"
+					name="level"
+					id={`l${index}`}
+					value={index}
+					checked={(form?.level ?? 1) === index}
+				/>
+				<label for={`l${index}`} class="level__label">{index}</label>
+			{/each}
+			<div class="level__indicator"></div>
+		</div>
+
+		<br />
+
+		<div class="submitwrapper">
+			<input type="submit" name="tod" value="truths" class="pure-button fuller-button blue" />
+			<p class="fuller-button">OR</p>
+			<input type="submit" name="tod" value="dares" class="pure-button fuller-button red" />
+		</div>
+	</form>
+
+	<h1>{form?.newDare}</h1>
+</div>
